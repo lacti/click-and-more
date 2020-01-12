9a0f862fda7fa39649ff149ca208c946aac05657
Dongwoon Son <dplusic@gmail.com>
2020-01-12 17:01:59 +0900

    Server traces whether level up is possible

--- api/src/game/model/tile.ts
+++ api/src/game/model/tile.ts
@@ -2,13 +2,14 @@ export interface ITileCore {
   i: number; // owner user index
   v: number; // click value
   l: number; // tile level
+  p: boolean; // level up possible
 }
 
 export const tileCoreEquals = (a: ITileCore, b: ITileCore): boolean =>
-  a.i === b.i && a.v === b.v && a.l === b.l;
+  a.i === b.i && a.v === b.v && a.l === b.l && a.p === b.p;
 
 export const tileCoreClone = (a: ITileCore): ITileCore =>
-  ({i: a.i, v: a.v, l: a.l});
+  ({i: a.i, v: a.v, l: a.l, p: a.p});
 
 export interface ITile extends ITileCore {
   growingTimer: number;
@@ -29,6 +30,7 @@ export const emptyTile = (): ITile => ({
   i: noOwnerIndex,
   v: 0,
   l: 0,
+  p: false,
   growingTimer: 0,
 });
 
@@ -43,6 +45,7 @@ const defaultChangingTile: ITile = {
   i: noOwnerIndex,
   v: 0,
   l: 0,
+  p: null, // unused
   growingTimer: 0,
 };
 
@@ -53,11 +56,13 @@ export const newTileChange = (change: IPos & Partial<ITile>): TileChange => ({ .
 export const updateTile = (tile: ITile, change: ITile): ITile => {
   const {value: newValue, owner: newOwner} = updateValueAndOwner(tile.v, change.v, tile.i, change.i);
   const newLevel = updateLevel(tile.l, change.l, tile.i, newOwner);
+  const newLevelUpPossible = updateLevelUpPossible(newLevel, newValue);
   const newGrowingTimer = updateGrowingTimer(tile.growingTimer, change.growingTimer, tile.i, newOwner);
   return {
     i: newOwner,
     v: newValue,
     l: newLevel,
+    p: newLevelUpPossible,
     growingTimer: newGrowingTimer,
   }
 };
@@ -83,6 +88,48 @@ const updateLevel = (oldLevel: number, changingLevel: number, oldOwner: number, 
     return oldLevel + changingLevel;
   } else {
     return 0;
+  }
+};
+
+const levelUpValueTable = {
+  0: 10,
+  1: 20,
+  2: 40,
+  3: 70,
+  4: 110,
+  5: 160,
+  6: 220,
+  7: 290,
+  8: 370,
+  9: 460,
+  10: 560,
+  11: 670,
+  12: 790,
+  13: 920,
+  14: 1060,
+  15: 1210,
+  16: 1370,
+  17: 1540,
+  18: 1720,
+  19: 1910,
+  20: 2110,
+  21: 2320,
+  22: 2540,
+  23: 2770,
+  24: 3010,
+  25: 3260,
+  26: 3520,
+  27: 3790,
+  28: 4070,
+  29: 4360,
+  30: 4660,
+};
+
+const updateLevelUpPossible = (newLevel: number, newValue: number): boolean => {
+  if (newLevel in levelUpValueTable) {
+    return newValue >= levelUpValueTable[newLevel];
+  } else {
+    return false;
   }
 };
 
