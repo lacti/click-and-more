ab5cacd5678fe49735a4869f5db758caac8ed24d
jaeyoung.choi <lactrious@gmail.com>
2020-01-20 00:05:28 +0900

    Add lobby stage

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,88 +1,71 @@
-import React, { useEffect, useState, useCallback } from "react";
-import useWebSocket from "react-use-websocket";
-import "./App.css";
-import {
-  IContext,
-  GameResponse,
-  updateContext,
-  GameRequest,
-  GameStage,
-  initialContext
-} from "./models";
-// import { coalesceClick } from "./events/coalesceClick";
-import TileBoard from "./components/TileBoard";
-import { ReadyStateEnum } from "react-use-websocket/dist/lib/use-websocket";
-import { logHook } from "./utils/logger";
+import * as React from "react";
+import Game from "./Game";
+import { IGameStart } from "./models";
+import sleep from "./utils/sleep";
 
-const webSocketUrl = process.env.REACT_APP_WEBSOCKET_URL!;
+const authUrl = process.env.REACT_APP_AUTH_URL!;
+const lobbyUrl = process.env.REACT_APP_LOBBY_URL!;
+const gameApplicationId = process.env.REACT_APP_GAME_APPLICATION_ID!;
+const userName = "Clicker";
 
-// const filterLevelUp = (context: IContext) => ({
-//   value,
-//   y,
-//   x
-// }: IClickRequest["data"]) => {
-//   const tile = context.board[y][x];
-//   const v = Math.floor(tile.v);
-//   if (tile.i === context.me && v % 10 === 0) {
-//   }
-// };
+async function authenticate(): Promise<IGameStart> {
+  if (process.env.REACT_APP_LOCAL === "1") {
+    const testStart = {
+      gameId: "local-test-" + Date.now(),
+      members: [{ memberId: "me" }]
+    };
+    fetch("http://localhost:3000/debug", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(testStart)
+    })
+      .then(console.log)
+      .catch(console.log);
+    await sleep(500);
+    return {
+      gameId: testStart.gameId,
+      playerId: testStart.members[0].memberId,
+      url: "ws://localhost:3001"
+    };
+  } else {
+    return new Promise<IGameStart>(async (resolve, reject) => {
+      const authToken = await fetch(authUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          name: userName,
+          applications: [gameApplicationId]
+        })
+      })
+        .then(token => token.text())
+        .catch(reject);
+      const ws = new WebSocket(
+        lobbyUrl + "?authorization=" + encodeURIComponent(`Bearer ${authToken}`)
+      );
+      ws.onerror = reject;
+      ws.onopen = () => {
+        ws.send(
+          JSON.stringify({ type: "match", application: gameApplicationId })
+        );
+      };
+      ws.onmessage = event => {
+        const message = JSON.parse(event.data) as IGameStart;
+        resolve(message);
+      };
+    });
+  }
+}
 
-const App: React.FC = () => {
-  const [socketUrl] = useState(webSocketUrl);
-  const [context, setContext] = useState<IContext>(initialContext);
-  const [sendMessage, lastMessage, readyState] = useWebSocket(socketUrl);
-  const sendRequest = useCallback(
-    (request: GameRequest) =>
-      sendMessage(JSON.stringify(logHook(`Request`)(request))),
-    [sendMessage]
-  );
-  const sendClickOne = useCallback(
-    (y: number, x: number) => {
-      if (context.stage !== GameStage.Running) {
-        return;
-      }
-      const tile = context.board[y][x];
-      const type = tile.i === context.me && tile.p ? "levelUp" : "click";
-      sendRequest({ type, data: [{ y, x, value: 1 }] });
-    },
-    [sendRequest, context]
-  );
-
-  const onResponse = useCallback(
-    (response: GameResponse) =>
-      setContext(logHook(`Context`)(updateContext(context, response))),
-    [context, setContext]
-  );
-  useEffect(
-    () =>
-      !!lastMessage
-        ? onResponse(
-            logHook(`Response`)(JSON.parse(lastMessage.data) as GameResponse)
-          )
-        : undefined,
-    // eslint-disable-next-line
-    [lastMessage]
-  );
-  useEffect(() => {
-    if (readyState === ReadyStateEnum.Open) {
-      sendRequest({ type: "load" });
-    }
-  }, [readyState, sendRequest]);
-
-  return context.stage === GameStage.Wait ? (
-    <div className="App">Loading... ({context.age})</div>
-  ) : (
-    <React.Fragment>
-      <div className="App">Age ({context.age})</div>
-      <TileBoard
-        board={context.board}
-        colors={context.colors}
-        me={context.me}
-        // onClick={coalesceClick(sendClick)}
-        onClick={sendClickOne}
-      />
-    </React.Fragment>
-  );
-};
-
-export default App;
+export default function App() {
+  const [start, setStart] = React.useState<IGameStart | null>(null);
+  React.useEffect(() => {
+    authenticate()
+      .then(setStart)
+      .catch(console.error);
+  }, []);
+  return start === null ? <div>Wait for other users</div> : <Game {...start} />;
+}
