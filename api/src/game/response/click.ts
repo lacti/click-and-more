416422efee3ebcb542d2c0604708cc9139a2359d
jaeyoung.choi <lactrious@gmail.com>
2020-01-29 22:48:43 +0900

    Make it error-tolerance

--- api/src/game/response/click.ts
+++ api/src/game/response/click.ts
@@ -1,3 +1,4 @@
+import logger from "../logger";
 import { Board, diffBoards, tileCoreEquals, TileSync } from "../model";
 import { broadcast, IRespondResult } from "./support/broadcast";
 
@@ -31,7 +32,7 @@ export class ClickBroadcast {
 
     // Make a request pending until sent current one.
     if (this.sending) {
-      console.debug(`Click broadcast`, `pending a request`);
+      logger.debug(`Click broadcast`, `pending a request`);
       this.pending = { newBoard, connectionIds };
       return {};
     }
@@ -41,32 +42,32 @@ export class ClickBroadcast {
     this.lastBoard = newBoard;
 
     // Skip if there is no changes.
-    const changes = diffBoards(
-      oldBoard,
-      newBoard,
-      tileCoreEquals,
-    );
+    const changes = diffBoards(oldBoard, newBoard, tileCoreEquals);
     if (changes.length === 0) {
-      // console.debug(`Click broadcast`, `no changes`);
+      // logger.debug(`Click broadcast`, `no changes`);
       return {};
     }
 
     // Send current changes.
-    console.debug(`Click broadcast`, `send changes`, JSON.stringify(changes));
+    logger.debug(`Click broadcast`, `send changes`, JSON.stringify(changes));
     this.sending = true;
-    const result = await broadcast<IClickBroadcast>(connectionIds, {
-      type: "click",
-      changes
-    });
-    this.sending = false;
-
-    // Return a result of broadcast if there is no pending request.
-    if (this.pending === undefined) {
-      return result;
+    try {
+      const result = await broadcast<IClickBroadcast>(connectionIds, {
+        type: "click",
+        changes
+      });
+      // Return a result of broadcast if there is no pending request.
+      if (this.pending === undefined) {
+        return result;
+      }
+    } catch (error) {
+      logger.error(`Cannot broadcast messages`, changes, error);
+    } finally {
+      this.sending = false;
     }
 
     // Or, process a pending request right now.
-    console.debug(`Click broadcast`, `process next changes`);
+    logger.debug(`Click broadcast`, `process next changes`);
     const oldPending = this.pending;
     this.pending = undefined;
     return this.broadcast(oldPending);
