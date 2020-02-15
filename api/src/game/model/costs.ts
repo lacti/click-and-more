67387d3da158361379ea56660629fa1121261410
jaeyoung.choi <lactrious@gmail.com>
2020-02-15 16:18:16 +0900

    Add costs

---
+++ api/src/game/model/costs.ts
@@ -0,0 +1,5 @@
+export const costToUpgradeDefence = 10;
+export const costToUpgradeOffence = 20;
+export const costToUpgradeProductivity = 5;
+export const costToUpgradeAttackRange = 25;
+export const costToAttack = 1;
